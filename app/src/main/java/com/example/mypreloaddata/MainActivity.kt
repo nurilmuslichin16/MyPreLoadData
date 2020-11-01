9a5f701cdf9d8a6c552f2d3f7d4d57bf4db770b1
Nuril Muslichin <nurilmuslichin16@gmail.com>
2020-11-01 20:45:08 +0700

    Belum selesai

---
+++ app/src/main/java/com/example/mypreloaddata/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.mypreloaddata
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mypreloaddata.adapter.MahasiswaAdapter
+import com.example.mypreloaddata.database.MahasiswaHelper
+import kotlinx.android.synthetic.main.activity_mahasiswa.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerview.layoutManager = LinearLayoutManager(this)
+        val mahasiswaAdapter = MahasiswaAdapter()
+        recyclerview.adapter = mahasiswaAdapter
+
+        val mahasiswaHelper = MahasiswaHelper(this)
+        mahasiswaHelper.open()
+        val mahasiswaModels = mahasiswaHelper.getAllData()
+        mahasiswaHelper.close()
+
+        mahasiswaAdapter.setData(mahasiswaModels)
+    }
+}
---
+++ app/src/main/java/com/example/mypreloaddata/database/MahasiswaHelper.kt
@@ -0,0 +1,93 @@
+package com.example.mypreloaddata.database
+
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteOpenHelper
+import android.provider.BaseColumns._ID
+import com.example.mypreloaddata.database.DatabaseContract.MahasiswaColumns.Companion.NAMA
+import com.example.mypreloaddata.database.DatabaseContract.MahasiswaColumns.Companion.NIM
+import com.example.mypreloaddata.database.DatabaseContract.TABLE_NAME
+import com.example.mypreloaddata.model.MahasiswaModel
+
+class MahasiswaHelper(context: Context) {
+
+    private val dataBaseHelper: DatabaseHelper = DatabaseHelper(context)
+    private lateinit var database: SQLiteDatabase
+
+    @Throws(SQLiteException::class)
+    fun open() {
+        database = dataBaseHelper.writableDatabase
+    }
+
+    fun close() {
+        dataBaseHelper.close()
+
+        if (database.isOpen)
+            database.close()
+    }
+
+    fun getAllData(): ArrayList<MahasiswaModel> {
+        val cursor = database.query(TABLE_NAME, null, null, null, null, null, "$_ID ASC", null)
+        cursor.moveToFirst()
+        val arrayList = ArrayList<MahasiswaModel>()
+        var mahasiswaModel: MahasiswaModel
+        if (cursor.count > 0) {
+            do {
+                mahasiswaModel = MahasiswaModel()
+                mahasiswaModel.id = cursor.getInt(cursor.getColumnIndexOrThrow(_ID))
+                mahasiswaModel.nama = cursor.getString(cursor.getColumnIndexOrThrow(NAMA))
+                mahasiswaModel.nim = cursor.getString(cursor.getColumnIndexOrThrow(NIM))
+
+                arrayList.add(mahasiswaModel)
+                cursor.moveToNext()
+            } while (!cursor.isAfterLast)
+        }
+        cursor.close()
+        return arrayList
+    }
+
+    fun insert(mahasiswaModel: MahasiswaModel): Long {
+        val initialValues = ContentValues()
+        initialValues.put(NAMA, mahasiswaModel.nama)
+        initialValues.put(NIM, mahasiswaModel.nim)
+        return database.insert(TABLE_NAME, null, initialValues)
+    }
+
+    fun getDataByNama(nama: String): ArrayList<MahasiswaModel> {
+        val cursor = database.query(TABLE_NAME, null, "$NAMA LIKE ?", arrayOf(nama), null, null, "$_ID ASC", null)
+        cursor.moveToFirst()
+        val arrayList = ArrayList<MahasiswaModel>()
+        var mahasiswaModel: MahasiswaModel
+        if (cursor.count > 0) {
+            do {
+                mahasiswaModel = MahasiswaModel()
+                mahasiswaModel.id = cursor.getInt(cursor.getColumnIndexOrThrow(_ID))
+                mahasiswaModel.nama = cursor.getString(cursor.getColumnIndexOrThrow(NAMA))
+                mahasiswaModel.nim = cursor.getString(cursor.getColumnIndexOrThrow(NIM))
+
+                arrayList.add(mahasiswaModel)
+                cursor.moveToNext()
+            } while (!cursor.isAfterLast)
+        }
+        cursor.close()
+        return arrayList
+    }
+
+    companion object {
+        private var INSTANCE: MahasiswaHelper? = null
+
+        fun getInstance(context: Context): MahasiswaHelper {
+            if (INSTANCE == null) {
+                synchronized(SQLiteOpenHelper::class.java) {
+                    if (INSTANCE == null) {
+                        INSTANCE = MahasiswaHelper(context)
+                    }
+                }
+            }
+            return INSTANCE as MahasiswaHelper
+        }
+    }
+
+}
